288c5e0087be8c24217dafd6886437f87b073607
sonofbossman <akingbadeeniola452@gmail.com>
2025-01-01 17:35:45 +0100

    Imported new dependencies in index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
-import connectToMongoDB from "./app/configuration/mongoDBconn.js";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
+import "express-async-errors";
 import "dotenv/config";
-//importing database routes
-import userRoutes from "./app/routes/userRoutes.js";
+import { router as authRouter } from "./app/routes/authRoutes.js";
+import { router as userRouter } from "./app/routes/userRoutes.js";
+import connectToMongoDB from "./app/configuration/mongoDBconn.js";
 import subscriptionRoutes from "./app/routes/subscriptionRoutes.js";
 import planRoutes from "./app/routes/planRoutes.js";
+import { notFound as notFoundMiddleware } from "./middleware/not-found.js";
+import { errorHandlerMiddleware } from "./middleware/error-handler.js";
 
 const app = express();
 app.use(express.json());
